18bd2a9caef5e4681d121eedacc8ea1441dc0067
ocfox <i@ocfox.me>
2026-03-04 10:11:44 +0800

    *

--- modules/base.nix
+++ modules/base.nix
@@ -25,7 +25,7 @@ in
             }
           ];
         };
-        tailscale.enable = true;
+        yggdrasil.enable = true;
       };
       hardware.enableRedistributableFirmware = true;
       environment.systemPackages = with pkgs; [
