735a4caee23f46724e18c20c77c01a62b5a41284
sarin <me@s4r.in>
2026-08-17 07:16:03 +0800

    mpv-handler: 0.3.12 -> 0.4.2
    Diff: https://github.com/akiirui/mpv-handler/compare/v0.3.12...v0.4.2

--- pkgs/mpv-handler.nix
+++ pkgs/mpv-handler.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mpv-handler";
-  version = "0.3.12";
+  version = "0.4.2";
 
   src = fetchFromGitHub {
     owner = "akiirui";
     repo = "mpv-handler";
     rev = "v${version}";
-    hash = "sha256-K+1yF3QrItn2HEnQYayAc48Qp2lPSKzwqCKX1MiSvnk=";
+    hash = "sha256-QoctjneJA7CdXqGm0ylAh9w6611vv2PD1fzS0exag5A=";
   };
 
-  cargoHash = "sha256-pYM7wrDDeT++NOl08zKuD0aUUbTiWJfGh9/dbdv69pk=";
+  cargoHash = "sha256-gKDkDLTLzC53obDd7pORsqP6DhORTbx6tvQ4jq61znQ=";
 
   postInstall = ''
     install -Dm444 -t $out/share/applications share/linux/mpv-handler.desktop
