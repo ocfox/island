bd226d98ba9c3cb55257b8a3a6d4378fd96f6792
sarin <me@s4r.in>
2026-08-18 20:40:26 +0800

    aqua: 0-unstable-2026-08-18 -> 0-unstable-2026-08-18
    Diff: https://codeberg.org/oxc/aqua.git/compare/84505039854044ee4b7999b24adfdc99d773bfa2...2d8b006075ca027ce30f2d9ac0e65610c5f739d4

--- pkgs/aqua.nix
+++ pkgs/aqua.nix
@@ -11,8 +11,8 @@ buildGoModule {
 
   src = fetchgit {
     url = "https://codeberg.org/oxc/aqua.git";
-    rev = "84505039854044ee4b7999b24adfdc99d773bfa2";
-    hash = "sha256-Zo9sktLyo3ZLl9WI8roL4WvnFQjtGn3DNNYEzjMxgiA=";
+    rev = "2d8b006075ca027ce30f2d9ac0e65610c5f739d4";
+    hash = "sha256-SV3u6jh0Whd22oJNKHHp/VoUa0zwoDoU66bA8WHriPw=";
   };
 
   vendorHash = "sha256-rSfdTqix9aDZihF20ahPAqPcSXpsTasEEjoD3ADtDeo=";
