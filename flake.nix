9b0741f57b0d24ade7ec3d46d4742f423f0318fc
sarin <me@s4r.in>
2026-09-11 16:07:41 +0800

    flake: deduplicate flake-parts and nixpkgs-lib inputs

--- flake.nix
+++ flake.nix
@@ -7,11 +7,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     import-tree.url = "github:vic/import-tree";
     kix = {
       url = "github:ocfox/kix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
     disko = {
       url = "github:nix-community/disko";
